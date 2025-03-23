c8452a975d932c853fd8761c7b32355d1e74a342
mhyrbra <mhyrbabapour@gmail.com>
2025-03-24 01:42:58 +0330

    1

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+
+function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 15 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      'نه',
+      'مطمئنی؟',
+      'اگه پسر خوبی باشم؟',
+      'تروخدا خوشگلم',
+      'بستنی کاکائویی می‌خرما',
+      'با سوخاری فراوان',
+      'لطفا دختر کوچولوی من',
+      '))):ولی آخه',
+      '!می‌میرما',
+      'من مردم',
+      'خب الان داری با روح مهیار حرف می‌زنی',
+      'راه بیا دیگه فرشته من',
+      ':((((',
+      ')):میو',
+      'نه وقتشه بیام بدزدمت😡',
+      '):ولی مامانی',
+    ];
+    if (noCount > phrases.length - 1) {
+      return phrases[phrases.length - 1];
+    }
+    return phrases[noCount];
+  };
+
+  return (
+    <div className='-mt-16 flex h-screen flex-col items-center justify-center'>
+      {yesPressed ? (
+        <>
+          <img src='https://gifdb.com/images/high/love-cute-white-puffy-bear-sticker-6370meeebdiv0b8s.webp' />
+          <div className='my-4 text-4xl font-bold'>هورا!! فسقلی من</div>
+        </>
+      ) : (
+        <>
+          <img
+            className='h-[300px]'
+            src='https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif'
+          />
+          <h1 className='my-4 text-3xl mb-8'>میشه همیشه مال من باشی؟</h1>
+          <div className='flex items-center'>
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              معلومه که آره
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=' rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700'
+            >
+              {noCount === 0 ? 'نه' : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
